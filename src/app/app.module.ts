0308f713ae3faaed51cc8239423462054ca0728d
Leandro Picoli <leandropicoli@live.com>
2020-09-26 10:01:16 -0300

    Create MyPets page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { PetsPageComponentComponent } from './pets-page-component/pets-page-component.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
     LoginPageComponent,
     ResetPasswordPageComponent,
     SignupPageComponent,
+    PetsPageComponentComponent,
   ],
   imports: [
     BrowserModule,
