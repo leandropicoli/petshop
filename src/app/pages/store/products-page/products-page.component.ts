bba1f7d4a8858bf6c97ba89646483cab91aa60e6
Leandro Picoli <leandropicoli@live.com>
2020-09-26 12:57:25 -0300

    Removing unnecesary files

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
-  styleUrls: ['./products-page.component.css']
 })
 export class ProductsPageComponent implements OnInit {
 
