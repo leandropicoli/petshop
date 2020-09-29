c44c88b441af2531789c87fbe39f100ea0461b0b
Leandro Picoli <leandropicoli@live.com>
2020-09-29 07:55:55 -0300

    Created ProductCard Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
-import { FramePageComponent } from './pages/master/frame.page';
+import { FramePageComponent } from './pages/master/frame.page'
+import { ProductCardComponent } from './components/store/product-card/product-card.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { FramePageComponent } from './pages/master/frame.page';
     PetsPageComponent,
     ProductsPageComponent,
     CartPageComponent,
-    FramePageComponent
+    FramePageComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
