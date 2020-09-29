da2a1511a90632b7c428d24ee965c8a6714d593d
Leandro Picoli <leandropicoli@live.com>
2020-09-29 07:46:07 -0300

    Get Products from api async

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
 })
 export class ProductsPageComponent implements OnInit {
+  public products$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.products$ = this.data.getProducts();
   }
 
 }
