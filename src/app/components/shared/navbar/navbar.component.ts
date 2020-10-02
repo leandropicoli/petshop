b5baf5fefeeadb01b092c9cb0d97c8b7ae240c2e
Leandro Picoli <leandropicoli@live.com>
2020-10-02 08:02:35 -0300

    Get current user on NavBar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { Security } from 'src/app/utils/security.util';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
+  public user: User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.user = Security.getUser();
   }
-
 }
